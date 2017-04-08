52ec524d03d6cb6e1fdc9985955ba604fdbdd721
benweizhu <benweizhu@gmail.com>
2017-04-08 12:10:09 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,44 @@
-var path = require('path');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const extractCSS = new ExtractTextPlugin('styles.css');
 
 module.exports = {
-	entry: './app/index.js',
+	resolve: {
+		extensions: ['.js', '.jsx', '.json']
+	},
+	entry: {
+		app: ['./app/index.jsx'],
+		vendor: ['react', 'react-dom', 'react-redux', 'babel-polyfill']
+	},
 	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist')
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel-loader'
+			},
+			{
+				test: /\.scss$/,
+				use: extractCSS.extract(['css-loader?minimize&modules&importLoaders=2&localIdentName=[name]__[local]', 'postcss-loader', 'sass-loader'])
+			}
+		]
+	},
+	plugins: [
+		extractCSS,
+		new HtmlWebpackPlugin({
+			template: 'app/index.html',
+			inject: false
+		})
+	],
+	devServer: {
+		contentBase: path.join(__dirname, "dist"),
+		compress: true,
+		port: 9000
 	}
 };
