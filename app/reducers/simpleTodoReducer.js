cf495f8d68bc0751647d42109bd30f98739d53e9
benweizhu <benweizhu@gmail.com>
2017-04-09 16:17:36 +0800

    add count to todo

--- app/reducers/simpleTodoReducer.js
+++ app/reducers/simpleTodoReducer.js
@@ -1,10 +1,13 @@
+let count = 0;
 export default (state = { todos: [] }, action) => {
   switch (action.type) {
     case 'ADD_TODO': {
+      count += 1;
       return Object.assign({}, state, {
         todos: [
           ...state.todos,
           {
+            id: count,
             text: action.todoText,
             complete: false
           }
