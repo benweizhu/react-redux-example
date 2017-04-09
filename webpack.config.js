9370a917a96a0890f8ce6653ebf9e9962804659a
benweizhu <benweizhu@gmail.com>
2017-04-09 09:50:46 +0800

    add 0.0.0.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,48 @@
 const path = require('path');
+const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const extractCSS = new ExtractTextPlugin('styles.css');
 
 module.exports = {
-	resolve: {
-		extensions: ['.js', '.jsx', '.json']
-	},
-	entry: {
-		app: ['./app/index.jsx'],
-		vendor: ['react', 'react-dom', 'react-redux', 'babel-polyfill']
-	},
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: '[name].bundle.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.jsx$/,
-				exclude: /(node_modules|bower_components)/,
-				loader: 'babel-loader'
-			},
-			{
-				test: /\.scss$/,
-				use: extractCSS.extract(['css-loader?minimize&modules&importLoaders=2&localIdentName=[name]__[local]', 'postcss-loader', 'sass-loader'])
-			}
-		]
-	},
-	plugins: [
-		extractCSS,
-		new HtmlWebpackPlugin({
-			template: 'app/index.html',
-			inject: false
-		})
-	],
-	devServer: {
-		contentBase: path.join(__dirname, "dist"),
-		compress: true,
-		port: 9000
-	}
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  },
+  entry: {
+    app: ['./app/index.jsx'],
+    vendor: ['react', 'react-dom', 'react-redux', 'babel-polyfill']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: extractCSS.extract(['css-loader?minimize&modules&importLoaders=2&localIdentName=[name]__[local]', 'postcss-loader', 'sass-loader'])
+      }
+    ]
+  },
+  plugins: [
+    extractCSS,
+    new HtmlWebpackPlugin({
+      template: 'app/index.html',
+      inject: false
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    host: '0.0.0.0',
+    hot: true
+  }
 };
